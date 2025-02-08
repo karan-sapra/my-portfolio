21d7b33f2597fd76d91151b4e252c6c0de2a0389
karan-sapra <karansapra683@gmail.com>
2025-02-08 20:09:18 +0530

    fixed

--- app.js
+++
@@ -1,4 +0,0 @@
-<div>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
-</div>
